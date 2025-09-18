747b57d054a4f6822ade3f6a62a3ea8e13db8b07
nikosiak123 <nikodnaj@gmail.com>
2025-09-18 22:13:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -485,7 +485,7 @@ def process_event(event_payload):
 
                 now = datetime.now(pytz.timezone(TIMEZONE))
                 
-                if now < nudge_time < (now + timedelta(minutes=1)):
+                if now < nudge_time < (now + timedelta(minutes=5)):
                     logging.info("Status to FOLLOW_UP_LATER. Data jest poprawna. Generuję spersonalizowane przypomnienie...")
                     
                     follow_up_message = get_gemini_response(history, prompt_details, is_follow_up=True)
